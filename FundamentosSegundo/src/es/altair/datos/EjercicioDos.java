dc0e5fa491fe7cf1313d0ffeb9ef0ba552d24aaa
Francisco Javier Ruano Vázquez <frv@altair.edu.es>
2017-09-28 13:01:26 +0200

    Fundamentos

--- FundamentosSegundo/src/es/altair/datos/EjercicioDos.java
+++ FundamentosSegundo/src/es/altair/datos/EjercicioDos.java
@@ -1,5 +1,7 @@
 package es.altair.datos;
 
+import java.util.Scanner;
+
 public class EjercicioDos {
 
 	// Escribir un programa que defina dos variables enteras para describir las
@@ -7,9 +9,22 @@ public class EjercicioDos {
 	// escribir en la pantalla las longitudes de los lados, el perímetro y el área
 	// del rectángulo
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner sc = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		System.out.println("Alto: ");
+		int alto = sc.nextInt();
+
+		System.out.println("Ancho: ");
+		int ancho = sc.nextInt();
+
+		int perimetro = (2 * alto) + (2 * ancho);
+		int area = alto * ancho;
+
+		System.out.println("El rectángulo de alto igual a " + alto + " y ancho igual a " + ancho
+				+ " tiene\n como perímetro el valor igual a " + perimetro + " y como \nárea el valor igual a " + area);
+		
+		sc.close();
 	}
 
 }
--- FundamentosSegundo/src/es/altair/datos/EjercicioTres.java
+++ FundamentosSegundo/src/es/altair/datos/EjercicioTres.java
@@ -1,5 +1,7 @@
 package es.altair.datos;
 
+import java.util.Scanner;
+
 public class EjercicioTres {
 
 	// Escriba un programa para calcular el consumo medio de un automóvil. Debe
@@ -11,9 +13,40 @@ public class EjercicioTres {
 	// solicitará el valor que indica el cuentakilómetros. Debe calcular el consumo
 	// por cada 100 km y el coste por kilómetro
 
+	private static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 1ª VEZ
+		System.out.println("Precio Combustible 1ª Vez: ");
+		double precioLitroUno = sc.nextDouble();
+		
+		System.out.println("Precio llenar depósito 1ª Vez: ");
+		double precioTotalUno = sc.nextDouble();
+		
+		System.out.println("Kilómetros contador 1ª Vez: ");
+		double kilometrosUno = sc.nextDouble();
+		
+		// 2ª VEZ
+		System.out.println("Precio Combustible 2ª Vez: ");
+		double precioLitroDos = sc.nextDouble();
+		
+		System.out.println("Precio llenar depósito 2ª Vez: ");
+		double precioTotalDos = sc.nextDouble();
+		
+		// 3ª VEZ
+		System.out.println("Kilómetros contador 3ª Vez: ");
+		double kilometrosTres = sc.nextDouble();
+		
+		double numeroDeKilometros = kilometrosTres - kilometrosUno;
+		
+		double litrosPrimera = precioTotalUno / precioLitroUno;
+		double litrosSegunda = precioTotalDos / precioLitroDos;
+		
+		double precioKilometro = (litrosPrimera + litrosSegunda) / numeroDeKilometros;
+		
+		System.out.println("Consumo a los 100 kms: " + (precioKilometro * 100));
+		
+		sc.close();
 	}
 
 }
