2d20f402666b848bee7e2c5cc43e06057d1efcce
Francisco Javier Ruano Vázquez <frv@altair.edu.es>
2017-10-19 14:26:24 +0200

    Ejercicios While

---
+++ EjerciciosWhile/src/es/altair/datos/Ejercicio01.java
@@ -0,0 +1,28 @@
+package es.altair.datos;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	// Debe solicitar un número positivo al usuario
+	// Muestre todos los números desde el solicitado hasta el 0
+	
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int num;
+		do {
+			System.out.println("Nº: ");
+			num = sc.nextInt();
+		} while (num < 0);
+		
+		while (num > 0) {
+			System.out.println(num);
+			num--;
+		}
+		
+		
+		sc.close();
+	}
+
+}
---
+++ EjerciciosWhile/src/es/altair/datos/Ejercicio03.java
@@ -0,0 +1,12 @@
+package es.altair.datos;
+
+public class Ejercicio03 {
+
+	// Programa que muestre el producto de los 10 primeros números impares
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
