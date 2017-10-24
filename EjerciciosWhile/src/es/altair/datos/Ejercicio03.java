210801e47580dcef3e2a5eed165d1fb76d5f27e8
Francisco Javier Ruano Vázquez <frv@altair.edu.es>
2017-10-24 12:32:37 +0200

    Ejercicios While

--- EjerciciosWhile/src/es/altair/datos/Ejercicio03.java
+++ EjerciciosWhile/src/es/altair/datos/Ejercicio03.java
@@ -1,12 +1,42 @@
 package es.altair.datos;
 
+import java.util.Scanner;
+
 public class Ejercicio03 {
 
-	// Programa que muestre el producto de los 10 primeros números impares
+	// Programa que muestre y calcule el producto de los n primeros números impares
+	
+	private static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		long producto = 1;
+		
+		System.out.println("Indique el nº de impares: ");
+		int num_impares = sc.nextInt();
+		int i = num_impares;
+		
+		int impar = 1;
+		while (num_impares > 0) {
+			System.out.println(impar);
+			
+			producto = producto * impar;
+			
+			impar+=2;
+						
+			num_impares--;
+		}
+		
+		System.out.println("Producto de los " + i + " primeros impares: " + producto);
+		
+//		for (int i = num_impares; i > 0; i--) {
+//			System.out.println(impar);
+//			
+//			producto *= impar;
+//			
+//			impar+=2;
+//		}
+		
+		sc.close();
 	}
 
 }
---
+++ EjerciciosWhile/src/es/altair/datos/Ejercicio05.java
@@ -0,0 +1,12 @@
+package es.altair.datos;
+
+public class Ejercicio05 {
+
+	// Pedir un número y calcular su factorial
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
