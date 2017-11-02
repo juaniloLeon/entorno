edb1e50d78fe6c65918fdae05c56fed18620d934
Francisco Javier Ruano Vázquez <frv@altair.edu.es>
2017-11-02 14:28:05 +0100

    Ejercicios While

---
+++ EjerciciosWhile/src/es/altair/datos/Ejercicio11.java
@@ -0,0 +1,17 @@
+package es.altair.datos;
+
+public class Ejercicio11 {
+
+	// Solicite un número y muestre el cuadrado del número
+	// El cuadrado de un número se obtiene de sumar tantos números impares como
+	// indique el número
+	// El cuadrado de 1 es 1
+	// El cuadrado de 2 es 4, es decir, 1 + 3
+	// El cuadrado de 3 es 9, es decir, 1 + 3 + 5
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
