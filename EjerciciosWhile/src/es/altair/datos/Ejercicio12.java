924b198b7c983882e5bc132a406a239d7135d6d4
Francisco Javier Ruano Vázquez <frv@altair.edu.es>
2017-11-07 11:46:43 +0100

    Nuevo Ejercicio While

---
+++ EjerciciosWhile/src/es/altair/datos/Ejercicio12.java
@@ -0,0 +1,14 @@
+package es.altair.datos;
+
+public class Ejercicio12 {
+
+	// Un número natural se dice perfecto si es igual a la suma de sus divisores
+	// propios (divisores menores que el número). El 6 es perfecto porque porque 6 =
+	// 1 + 2 + 3. El 28 es perfecto porque 28 = 1 + 2 + 4 + 7 + 14
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
