aca2f6af3fa20ff4d99822ed616fb870f14638a6
Francisco Javier Ruano Vázquez <frv@altair.edu.es>
2017-11-28 12:26:02 +0100

    Ejercicios

---
+++ EjerciciosTablas/src/datos/Ejercicio02.java
@@ -0,0 +1,14 @@
+package datos;
+
+public class Ejercicio02 {
+
+	// Leer n números ordenados crecientemente. Pedir al usuario un número y
+	// buscarlo en la tabla. Se debe mostrar la posición en la que se encuentra. Si
+	// no está, indicarlo con un mensaje
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
