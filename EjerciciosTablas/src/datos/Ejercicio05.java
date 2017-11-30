b061aab877abfcfd4a33b7fe87ef2172c02c5d23
Francisco Javier Ruano Vázquez <frv@altair.edu.es>
2017-11-30 14:30:27 +0100

    Ejercicios

--- EjerciciosTablas/src/datos/Ejercicio05.java
+++ EjerciciosTablas/src/datos/Ejercicio05.java
@@ -1,14 +1,55 @@
 package datos;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio05 {
 
 	// Crear una tabla de n números y desplazarla una posición hacia abajo, es
 	// decir, el primero pasa a ser el segundo, el segundo pasa a ser el tercero y
 	// así sucesivamente. El último pasa a ser el primero
 
+	private static Scanner sc = new Scanner(System.in);
+	
+	private static int numeros [];
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int tamanyo;
+		do {
+			System.out.println("Nº de Elementos: ");
+			tamanyo = sc.nextInt();
+		} while (tamanyo <= 0);
+		
+		numeros = new int[tamanyo];
+		
+		iniciar();
+		
+		mostrar();
+		
+		desplazar();
+		
+		mostrar();
+		
+		sc.close();
+	}
 
+	private static void desplazar() {
+		int primero = numeros[0];
+		for (int i = 1; i < numeros.length; i++) {
+			numeros[i-1] = numeros[i];
+		}
+		numeros[numeros.length -1] = primero;
+	}
+
+	private static void mostrar() {
+		System.out.println("NÚMEROS: " + Arrays.toString(numeros));
+	}
+
+	private static void iniciar() {
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = new Random().nextInt(50);
+		}
 	}
 
 }
